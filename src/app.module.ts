17a680eba9f1562920774bfd278aa3abe4f1b65c
Pratheek <kkpratheek@gmail.com>
2025-12-20 17:43:30 +0530

    Remove sensitive data from code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { MatchesModule } from './matches/matches.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayerModule } from './player/player.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MatchesModule,
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MatchesModule,
     MongooseModule.forRoot(
-      'mongodb+srv://cricintel_user:cricintel_password@cricintelcluster.pz2zlat.mongodb.net/cricintel'
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cricintelcluster.pz2zlat.mongodb.net/cricintel`
     ),
     PlayerModule,
   ]
